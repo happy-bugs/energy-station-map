9f7ef110b1483034ec8364ae633821d85139a33c
bodol4748 <55184097+bodol4748@users.noreply.github.com>
2025-09-07 20:39:07 +0900

    init: project 생성

---
+++ src/main/java/happy/bug/energy_station_map/EnergyStationMapApplication.java
@@ -0,0 +1,13 @@
+package happy.bug.energy_station_map;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnergyStationMapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnergyStationMapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/happy/bug/energy_station_map/EnergyStationMapApplicationTests.java
@@ -0,0 +1,13 @@
+package happy.bug.energy_station_map;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnergyStationMapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
